48da69099e82ecd9fb1ce773c5396b1bc3105e8c
Cinzia Borello <cinziaborello@gmail.com>
2021-01-08 17:58:16 -0800

    Reorganize client folder files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
-import './App.css';
+import React from 'react';
+import AppGrid from './viewComponents/AppGrid';
 
-function App() {
-  const [data, setData] = useState('');
-
-  const getData = () => {
-    fetch('/api')
-      .then((result) => result.text())
-      .then((res) => setData(res));
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button color="primary" onClick={getData}>Click Me For Date</Button>
-        {data}
-      </header>
-    </div>
-  );
-}
+const App = ():JSX.Element => (
+  <div>
+    <AppGrid />
+  </div>
+);
 
 export default App;
